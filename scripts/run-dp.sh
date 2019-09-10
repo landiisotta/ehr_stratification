601b9cf2cd44efc547d2aed0f395bca820287214
Riccardo Miotto <riccardo.miotto@mssm.edu>
2019-09-10 12:08:31 -0400

    added script for dp

---
+++ scripts/run-dp.sh
@@ -0,0 +1,19 @@
+#! /bin/zsh
+
+clear
+projdir=..
+datadir=$projdir/data
+
+indir=$datadir/ehr-804371-test-2
+outdir=$indir/dp-encodings
+
+test_set=$1
+
+if [ ! -z "$test_set" ]
+then
+    test_set="--test_set $test_set"
+fi
+
+../../stratification_ILRM/myvenv/bin/python -u $projdir/src/baselines.py $indir $outdir $test_set
+
+# -test1/test2
